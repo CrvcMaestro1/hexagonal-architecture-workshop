d94dad742a526fa6ee257cbd1eefa66990360698
Christian Vera <christian.vera@tipti.market>
2022-10-16 09:06:04 -0500

    feat: remove name validator in room model

--- src/domain/room/room.py
+++ src/domain/room/room.py
@@ -11,13 +11,6 @@ class RoomBase(DomainModel):
     class Config:
         orm_mode = True
 
-    @validator("name", pre=True, always=True, check_fields=False)
-    def name_must_only_have_letters_numbers_and_spaces(cls, v: Any) -> Any:
-        match = re.match("^[a-zA-Z0-9 ]*$", v)
-        if not match:
-            raise ValueError("Name must only have letters, number and spaces")
-        return v
-
     @validator("capacity", pre=True, always=True, check_fields=False)
     def capacity_must_greater_than_zero(cls, v: Any) -> Any:
         if v < 0:
