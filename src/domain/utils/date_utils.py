de5e11070e05fe332778dd8ba325f4f6de4ffef9
Christian Vera <christian.vera@tipti.market>
2022-10-16 09:07:31 -0500

    feat: improve str to date util

--- src/domain/utils/date_utils.py
+++ src/domain/utils/date_utils.py
@@ -1,13 +1,19 @@
 from datetime import date, datetime
+from typing import Optional
 
-DATE_FORMAT = "%Y-%m-%d"
+DATE_FORMATS = ["%Y-%m-%d", "%Y-%m-%dT%H:%M:%S.%fZ"]
 
 
 def subtract_days_from_dates(greater_date: date, minor_date: date) -> int:
     return (greater_date - minor_date).days
 
 
-def str_to_date(date_str: str | date) -> date:
+def str_to_date(date_str: str | date) -> Optional[date]:
     if isinstance(date_str, date):
         return date_str
-    return datetime.strptime(date_str, DATE_FORMAT).date()
+    for date_format in DATE_FORMATS:
+        try:
+            return datetime.strptime(date_str, date_format).date()
+        except ValueError:
+            pass
+    return None
